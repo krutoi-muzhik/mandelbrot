ab52d1c6dc8924eb23c151ea5b4158cf48330ad7
krutoi-muzhik <ddaniild@list.ru>
2022-04-19 02:44:28 +0300

    started working on optimization

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 TARGET = mandelbrot
+BOOST = MandelbrotBoost
 CC = gcc
 GLLIBS = -lopengl32 -lglfw3 -lgdi32 -lglew32
 
+.PHONY: boost
+
 all: $(TARGET)
+
+boost: main.c $(BOOST).c $(TARGET).h
+	$(CC) main.c $(BOOST).c $(GLLIBS) -o $(TARGET).exe
 
 $(TARGET): $(TARGET).c $(TARGET).h
 	$(CC) main.c $(TARGET).c $(GLLIBS) -o $(TARGET).exe
---
+++ MandelbrotBoost.c
@@ -0,0 +1,178 @@
+#include <intrin.h>
+#include <immintrin.h>
+#include "mandelbrot.h"
+
+enum DEFAULT_VALS {
+	SZX = 960,
+	SZY = 540,
+	ITERS = 200,
+	ZOOM = 10
+};
+
+int WIDTH = SZX;
+int HEIGHT = SZY;
+double zoom = ZOOM;
+int dragging = 0;
+double offsetX = -480.0, offsetY = 270.0;
+double posX = 0.0, posY = 0;
+size_t iterations = ITERS;
+
+#define C_re re / zoom
+#define C_im im / zoom
+
+void do_render () {
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBegin(GL_POINTS);
+		__m256d _x, _y, _cre, _cim, _x_tmp, _y_tmp, _x2, _y2, _two, _cmp_vector;
+
+		_two = _mm256_set1_pd (2.0);
+
+		for (ssize_t re = offsetX; re < offsetX + WIDTH; re ++) {
+
+			_cre = _mm256_set1_pd (re / zoom);
+
+			for (ssize_t im = offsetY; im > offsetY - HEIGHT; im --) {
+				size_t iter = 0;
+				while ((iter < iterations) && _mm256_movemask_pd (_cmp_vector)) {
+					_x2 = _mm256_mul_pd (_x, _x);			// x * x
+					_y2 = _mm256_mul_pd (_y, _y);			// y * y
+
+					_x_tmp = _mm256_sub_pd (_x2, _y2);		// x_tmp = x * x - y * y
+					_x_tmp = _mm256_add_pd (_x_tmp, _cre);
+
+					_y_tmp = _mm256_mul_pd (_x, _y);		// y_tmp = x * y
+					_y_tmp = _mm256_fmadd_pd (_y_tmp, _two, _cim);
+
+					_x = _x_tmp;
+					_y = _y_tmp;
+
+					_cmp_vector = _mm256_cmp_pd (_mm256_add_pd (_x2, _y2), _two, _CMP_LT_OS);
+
+					iter++;
+				}
+
+				// printf ("offsetX = %lf, offsetY = %lf\n", offsetX, offsetY);
+
+				// printf ("C_re = %lf, C_im = %lf\n", C_re, C_im);
+
+				// printf ("x = %lf, y = %lf, iter = %d\n", re, im, iter);
+
+				float r = 0.0 + iter / (5 * iterations);
+				float g = 0.1 - iter / (3 * iterations);
+				float b = 0.3 + iter / (1.5 * iterations);
+
+				glColor3f(r, g, b);
+
+				// printf ("x = %lf y = %lf\n", (re - offsetX) * 2 / WIDTH - 1, (offsetY - im) * 2 / HEIGHT - 1);
+				glVertex2d((re - offsetX) * 2 / WIDTH - 1, (offsetY - im) * 2 / HEIGHT - 1);
+			}
+		}
+	glEnd();
+	return;
+}
+
+void framebuffer_size_callback (GLFWwindow *window, int width, int height) {
+	glViewport(0, 0, width, height);
+	glfwGetWindowSize (window, &WIDTH, &HEIGHT);
+	return;
+}
+
+GLFWwindow *initialize_window () {
+	GLFWwindow *Window;
+	if (!glfwInit ())
+		exit (EXIT_FAILURE);
+
+	Window = glfwCreateWindow (SZX, SZY, "MANDELBROT", NULL, NULL);
+	if (!Window) {
+		glfwTerminate ();
+		exit (EXIT_FAILURE);
+	}
+	glfwMakeContextCurrent (Window);
+
+	if (glewInit () != GLEW_OK) {
+		glfwTerminate ();
+		exit (EXIT_FAILURE);
+	}
+	glfwSetFramebufferSizeCallback (Window, framebuffer_size_callback);
+	glfwSetKeyCallback(Window, key_callback);
+	glfwSetCursorPosCallback(Window, cursor_position_callback);
+	glfwSetMouseButtonCallback(Window, mouse_button_callback);
+	glfwSetScrollCallback(Window, scroll_callback);
+	return Window;
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		glfwGetCursorPos(window, &posX, &posY);
+		dragging = 1;
+	}
+	else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+		dragging = 0;
+	return;
+}
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
+	if (dragging) {
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		offsetX -= (xpos - posX);
+		offsetY -= (ypos - posY);
+
+		posX = xpos;
+		posY = ypos;
+	}
+	return;
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+	if (yoffset != 0) {
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+		if (yoffset < 0) {
+			zoom /= 1.2;
+			offsetY += (ypos - HEIGHT / 2) * (1 - 1.2) / zoom;
+			offsetX += (xpos - WIDTH / 2) * (1 - 1.2) / zoom;
+		} else {
+			zoom *= 1.2;
+			offsetY += (ypos - HEIGHT / 2) * (1 - 1 / 1.2) / zoom;
+			offsetX += (xpos - WIDTH / 2) * (1 - 1 / 1.2) / zoom;
+		}
+	}
+	return;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, 1);
+	}
+
+	if (key == GLFW_KEY_KP_0 && action == GLFW_PRESS) {
+		iterations = ITERS;
+		zoom = ZOOM;
+		offsetX = 0.0;
+		offsetY = 0.0;
+	}
+
+	if (key == GLFW_KEY_A && action == GLFW_PRESS)
+		offsetX -= WIDTH / 5;
+	else if (key == GLFW_KEY_D && action == GLFW_PRESS)
+		offsetX += WIDTH / 5;
+	else if (key == GLFW_KEY_W && action == GLFW_PRESS)
+		offsetY -= HEIGHT / 5;
+	else if (key == GLFW_KEY_S && action == GLFW_PRESS)
+		offsetY += HEIGHT / 5;
+
+	if (key == GLFW_KEY_KP_ADD && action == GLFW_PRESS)
+		zoom *= 2;
+	else if (key == GLFW_KEY_KP_SUBTRACT && action == GLFW_PRESS)
+		zoom /= 2;
+
+	if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+		iterations += 50;
+	else if (key == GLFW_KEY_E && action == GLFW_PRESS && iterations >= 100)
+		iterations -= 50;
+}
